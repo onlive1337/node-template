11839c4409727a6cb8c49bea70c0236081c65ac1
onlive1337 <tabdulov2006@gmail.com>
2024-05-08 15:26:57 +0500

    folder and files with random numbers

--- date.js
+++
@@ -1,6 +0,0 @@
-function getCurrentDate() {
-  const currentDate = new Date();
-  return `it's time now ${currentDate.getFullYear()}:${currentDate.getMonth() + 1}:${currentDate.getDate()}`;
-}
-
-module.exports = getCurrentDate;
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const fetchAndLogPostsFetch = require("./fetch");
+const fs = require('fs').promises;
 
-fetchAndLogPostsFetch();
+async function createFolderAndFiles() {
+ try {
+  const folderName = 'random_numbers';
+  await fs.mkdir(folderName);
+
+  for (let i = 1; i <= 3; i++) {
+    const randomNumbers = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)).join('');
+    await fs.writeFile(`${folderName}/file${i}.txt`, randomNumbers);
+  }
+  console.log('Folder and files has created');
+ } catch (error) {
+  console.error('Error', error);
+ }
+}
+
+createFolderAndFiles();
